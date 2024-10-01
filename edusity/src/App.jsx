3d6cc124f7393dc9d56a1772b59aaf4edf02fe21
sarathkumar s <sarathkumar93622@gmail.com>
2024-10-02 01:00:56 +0530

    completed title and program section working on ABOUT component

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer'/>
+      <Programs/>
+      </div>
+      
     </div>
   )
 }
