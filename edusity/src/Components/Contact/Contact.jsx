0bc39b9f1a05abce00df89cd882656f68fecf3ec
sarathkumar s <sarathkumar93622@gmail.com>
2024-10-02 13:00:30 +0530

    left section of contact page is completed, Working on the right section

---
+++ edusity/src/Components/Contact/Contact.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <div className="contact-col">
+        <h3>Send us a message <img src={msg_icon} alt='' /></h3>
+        <p>Feel free to reach out through contact form or find our contact 
+            information below. Your feedback, questions, and suggestions are 
+            important to us as we strive to provide exceptional service to our 
+            university community.
+            <ul>
+                <li><img src={mail_icon} alt=''/>Contact@edusity.dev</li>
+                <li><img src={phone_icon} alt=''/>+1 123-46-7890</li>
+                <li><img src={location_icon} alt=''/>77 Masachusetts Ave, Cambridge<br/> MA 02139, United States</li>
+            </ul>
+        </p>
+      </div>
+      <div className="contact-col"></div>
+    </div>
+  )
+}
+
+export default Contact
